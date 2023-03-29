d60796d1ade00370eaf8b3aa3cd4a02218858e18
Stiliyan Dyankov <stiliandiankov0704@gmail.com>
2023-03-29 23:00:20 +0300

    Introduce password hashing in register

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -14,18 +14,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authDB_1 = require("../db/authDB");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const router = express_1.default.Router();
 router.get("/", (req, res) => {
     res.status(200).send("Auth router works");
 });
 router.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     console.log("req body post to api", req.body);
+    // hash password
+    let userCredentials = Object.assign({}, req.body);
+    const salt = yield bcrypt_1.default.genSalt();
+    userCredentials.password = yield bcrypt_1.default.hash(userCredentials.password, salt);
     // create user
-    const result = yield (0, authDB_1.createUser)(req.body);
+    const result = yield (0, authDB_1.createUser)(userCredentials);
     console.log("result register api post", result);
+    // handle error case from createUser()
     if (result.name) {
-        return res.status(500).send("INTERNAL ERROR!!! Couldn't create new user.");
+        return res
+            .status(500)
+            .send("INTERNAL ERROR!!! Couldn't create new user.");
     }
+    // handle success case from createUser()
     return res.status(200).send(result);
 }));
 exports.default = router;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,25 +1,40 @@
 import express, { Request, Response } from "express";
 import { createUser, User } from "../db/authDB";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-
-
 router.get("/", (req: Request, res: Response) => {
     res.status(200).send("Auth router works");
-})
+});
 
 router.post("/register", async (req: Request, res: Response) => {
     console.log("req body post to api", req.body);
+
     
+
+    // hash password
+    let userCredentials = { ...req.body };
+    const salt = await bcrypt.genSalt();
+    userCredentials.password = await bcrypt.hash(
+        userCredentials.password,
+        salt
+    );
+
     // create user
-    const result = await createUser(req.body);
-    
+    const result = await createUser(userCredentials);
+
     console.log("result register api post", result);
-    if((result as Error).name) {
-        return res.status(500).send("INTERNAL ERROR!!! Couldn't create new user.");
+
+    // handle error case from createUser()
+    if ((result as Error).name) {
+        return res
+            .status(500)
+            .send("INTERNAL ERROR!!! Couldn't create new user.");
     }
+
+    // handle success case from createUser()
     return res.status(200).send(result as User);
-})
+});
 
 export default router;
